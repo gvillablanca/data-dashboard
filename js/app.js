559860353a9dd241000100787f6a7b322bb1aa0f
gvillablanca <g.villalobo.m@gmail.com>
2017-11-17 15:46:42 -0300

    cambio filtros

--- js/app.js
+++ js/app.js
@@ -4,3 +4,18 @@
 
 // Puedes hacer uso de la base de datos a través de la variable `data`
 console.log(data);
+
+
+function click (){
+	var encabezado = document.getElementById('encabezado');
+	var user = document.getElementById('user');
+
+	user.addEventListener('click', function(){
+		var div = document.createElements('div');
+
+		div.setAtritbutte('id', 'caja');
+		div.style.backgroundImage = "url(../../assets/images/perfil.png)";
+
+		encabezado.appendChild(div);
+	})
+}
